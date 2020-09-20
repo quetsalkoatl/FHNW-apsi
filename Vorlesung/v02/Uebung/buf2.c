b7fe85154ac4ba15ea865666f4be1957bf548e44
wagner <wagner@m2.tansi.org>
2020-09-20 22:15:56 +0200

    final fuer MOntag

---
+++ Vorlesung/v02/Uebung/buf2.c
@@ -0,0 +1,15 @@
+
+#include <string.h>
+#include <stdio.h>
+
+void test (int argc, char *argv[]) {
+  char s2[4] = "yes";    // set s2 to "yes"
+  char s1[4] = "abc";    // set s1 to "abc"
+  strcpy(s1, argv[1]);   // copy argv[1] into s1
+  puts(s2);              // print s2
+}
+
+int main (int argc, char *argv[]) {
+  test(argc, argv);
+  puts("end\n");
+}
