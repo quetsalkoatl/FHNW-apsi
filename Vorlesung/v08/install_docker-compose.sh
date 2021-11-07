959a19af89293d462a4e6f8c70dcbd813fc6c439
Rolf Wagner <rolf.wagner@fort-it.ch>
2021-11-07 16:56:05 +0100

    WebGoat

---
+++ Vorlesung/v08/install_docker-compose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing docker-compose on the machine"
+sudo curl -L "https://github.com/docker/compose/releases/download/2.1.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "Successfully installed, you may now run the docker-compose file using: docker-compose up"
